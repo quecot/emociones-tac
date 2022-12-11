fba7ec6b01961e7be4afd1230cf75a2985f2e635
Francesc Vila <francescvilasubias@gmail.com>
2022-12-11 17:24:25 +0100

    feat: basic survey with survey-core

---
+++ src/json.ts
@@ -0,0 +1,164 @@
+export const json = {
+  "elements": [
+    {
+      "type": "matrix",
+      "name": "questionnaire",
+      "title": "Indica tu grado de acuerdo con las siguientes afirmaciones:",
+      "columns": [
+        {
+          "value": 0,
+          "text": "Muy en desacuerdo"
+        },
+        {
+          "value": 1,
+          "text": "En desacuerdo"
+        },
+        {
+          "value": 2,
+          "text": "Neutral"
+        },
+        {
+          "value": 3,
+          "text": "De acuerdo"
+        },
+        {
+          "value": 4,
+          "text": "Muy de acuerdo"
+        },        
+      ],
+      "rows": [
+        {
+          "value": "alegria1",
+          "text": "¿Te sientes feliz y contento en este momento?"
+        },
+        {
+          "value": "alegria2",
+          "text": "¿Estás experimentando una sensación de satisfacción o bienestar?"
+        },
+        {
+          "value": "alegria3",
+          "text": "¿Estás disfrutando de una actividad o situación?"
+        },
+        {
+          "value": "alegria4",
+          "text": "¿Te sientes lleno de energía?"
+        },
+        {
+          "value": "sorpresa1",
+          "text": "¿Estás sorprendido por algo que acaba de suceder?"
+        },
+        {
+          "value": "sorpresa2",
+          "text": "¿Te sientes desconcertado o confundido ante algo?"
+        },
+        {
+          "value": "sorpresa3",
+          "text": "¿Estás experimentando una sensación de incertidumbre?"
+        },
+        {
+          "value": "sorpresa4",
+          "text": "¿Estás experimentando una emoción fuerte que te desconcierta?"
+        },
+        {
+          "value": "miedo1",
+          "text": "¿Sientes miedo o inquietud ante algún peligro o amenaza?"
+        },
+        {
+          "value": "miedo2",
+          "text": "¿Estás preocupado o nervioso por algo que pueda suceder en el futuro?"
+        },
+        {
+          "value": "miedo3",
+          "text": "¿Estás experimentando una sensación de angustia o pánico?"
+        },
+        {
+          "value": "miedo4",
+          "text": "¿Tienes la sensación de que algo malo va a pasar?"
+        },
+        {
+          "value": "tristeza1",
+          "text": "¿Te sientes triste o deprimido?"
+        },
+        {
+          "value": "tristeza2",
+          "text": "¿Estás experimentando una sensación de pérdida o desesperación?"
+        },
+        {
+          "value": "tristeza3",
+          "text": "¿Echas algo o a alguien de menos?"
+        },
+        {
+          "value": "tristeza4",
+          "text": "¿Te sientes abrumado o tienes ganas de llorar?"
+        },
+        {
+          "value": "ira1",
+          "text": "¿Estás enojado o enfadado por algo o alguien?"
+        },
+        {
+          "value": "ira2",
+          "text": "¿Sientes indignación o ira ante algo o alguien?"
+        },
+        {
+          "value": "ira3",
+          "text": "¿Te sientes irritado o frustrado por algo en particular?"
+        },
+        {
+          "value": "ira4",
+          "text": "¿Tienes la sensación de que estás a punto de explotar o perder el control?"
+        },
+        {
+          "value": "anticipacion1",
+          "text": "¿Te sientes expectante o ansioso por lo que va a ocurrir?"
+        },
+        {
+          "value": "anticipacion2",
+          "text": "¿Tienes la sensación de que estás a punto de experimentar algo emocionante o importante?"
+        },
+        {
+          "value": "anticipacion3",
+          "text": "¿Estás constantemente prestando atención a señales o indicios de lo que va a suceder?"
+        },
+        {
+          "value": "anticipacion4",
+          "text": "¿Te sientes entusiasmado o emocionado por lo que viene en el futuro?"
+        },
+        {
+          "value": "confianza1",
+          "text": "¿Te sientes seguro de ti mismo en este momento?"
+        },
+        {
+          "value": "confianza2",
+          "text": "¿Tienes la sensación de que tienes el control de la situación?"
+        },
+        {
+          "value": "confianza3",
+          "text": "¿Tienes la sensación de que puedes confiar en ti mismo para tomar las decisiones correctas?"
+        },
+        {
+          "value": "confianza4",
+          "text": "¿Te sientes capaz de enfrentar cualquier desafío o situación?"
+        },
+        {
+          "value": "asco1",
+          "text": "¿Estás experimentando una sensación de rechazo?"
+        },
+        {
+          "value": "asco2",
+          "text": "¿Tienes la sensación de que algo es desagradable?"
+        },
+        {
+          "value": "asco3",
+          "text": "¿Estás experimentando una sensación de mareo?"
+        },
+        {
+          "value": "asco4",
+          "text": "¿Estás experimentando una sensación de náusea o malestar estomacal?"
+        },
+      ],
+      "alternateRows": true,
+      "isAllRowRequired": true,
+    }
+  ],
+  "showQuestionNumbers": "off"
+};
