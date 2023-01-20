d8b1693e3b3a45ac4194089a59a020ff994ea34c
Francesc Vila <francescvilasubias@gmail.com>
2023-01-20 11:09:18 +0100

    feat: add license and authors

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -6,10 +6,16 @@ interface Props {
 
 const Welcome: React.FC<Props> = ({ setStatus }) => {
   return (
-    <div className='welcome-div'>
-      <h1>Bienvenid@</h1>
-      <p>El siguiente test te va a servir para descubrir cuál es tu estado emocional presente. A través de una serie de preguntas, podrás evaluar cómo te sientes en este momento y cómo están afectando tus emociones tu día a día. Es importante tener en cuenta que este test es solo una herramienta de autodiagnóstico y no debe tomarse como una evaluación profesional o un sustituto de asesoramiento psicológico. Sin embargo, puede ser una forma útil de identificar patrones o problemas emocionales que puedan requerir más atención y apoyo. Recuerda que es normal tener altibajos emocionales y es importante aprender a manejarlos de manera saludable.</p>
-      <button type="button" onClick={() => setStatus('taking-test')}>Empezar</button>
+    <div>
+      <div className='welcome-div'>
+        <h1>Bienvenid@</h1>
+        <p>El siguiente test te va a servir para descubrir cuál es tu <b>estado emocional presente</b>. A través de una serie de preguntas, podrás evaluar <b>cómo te sientes en este momento</b> y cómo están afectando tus emociones tu día a día. Es importante tener en cuenta que este test es solo una herramienta de autodiagnóstico y no debe tomarse como una evaluación profesional o un sustituto de asesoramiento psicológico. Sin embargo, puede ser una forma útil de identificar patrones o problemas emocionales que puedan requerir más atención y apoyo. Recuerda que es normal tener altibajos emocionales y es importante aprender a manejarlos de manera saludable.</p>
+        <button type="button" onClick={() => setStatus('taking-test')}>Empezar</button>
+      </div>
+      <div className="creative-commons">
+        <p>2023 | Martina Bastida, Laia Casanovas, Ari Gotta i Francesc Vila | TAC | Pedagogía UAB</p>
+        <img src="src/assets/creative_commons.png" width="140px"></img>
+      </div>
     </div>
   )
 }
